4aee29fbe4ba00b6a1aca2c9a91554e63f2b163e
Stephen Hilt <shooter000@gmail.com>
2016-04-07 12:45:51 -0400

    Create rss_slack.py

---
+++ rss_slack.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+#
+#
+# Author: Stephen J. Hilt (http://gitub.com/sjhilt)
+# Purpose: To read RSS feeds and post to a given slack channel
+#
+#
+import feedparser
+from slacker import Slacker
+import time
+
+#Setup to pull RSS feed example is ICS-CERT Alerts
+rss =feedparser.parse('https://ics-cert.us-cert.gov/alerts/alerts.xml')
+#setup slack via Slacker
+slack = Slacker('YOUR_API_KEY')
+# create current time based off RSS feeds format 
+# the minus -4 is for an offset of GMT to East Cost minus an hour since i'm running this 
+# as a cron job every how, so this will pull everything from the past hour
+day = time.strftime("%d")
+month = time.strftime("%b")
+year = time.strftime("%Y")
+hour = time.strftime("%H")
+
+if int(time.strftime("%H"))+4 < 23:
+        date_time = ", {:02d} {} {} {:02d}:".format(int(day), month, year, int(hour) + 4)
+else:
+        hour = abs(24 - (int(hour) + 4))
+        date_time = ", {:02d} {} {} {:02d}:".format(int(day)+1, month, year, int(hour))
+
+count = 0 
+# set a counter to 0 to loop through all the RSS feed entries
+count = 0
+while count < len(rss['entries']):
+        # pull the entries published date/time
+        published = rss['entries'][count]['published']
+        # if from the last hour
+        if date_time in published:
+                # Title, description, and link to be posted
+                title = rss['entries'][count]['title'].encode('ascii', 'ignore')
+                desc = rss['entries'][count]['description'].encode('ascii', 'ignore')
+                link = rss['entries'][count]['link']
+                # send message to slack
+                slack.chat.post_message('#random', "_*{}*_\n{}\n{}\n".format(title,desc,link), username="RSSbot", icon_emoji=":rage:")
+        count += 1
