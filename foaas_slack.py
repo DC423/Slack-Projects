47a3dec40b89175236dfc65985ecf97237666cf4
Stephen Hilt <shooter000@gmail.com>
2016-05-03 21:30:24 -0400

    Update foaas_slack.py

--- foaas_slack.py
+++ foaas_slack.py
@@ -23,5 +23,13 @@ url = "\'http://foaas.com/{}/@{}/{}\' -H \'Accept: application/json\'".format(co
 response = os.popen('curl -s {}'.format(url)).read()
 # pares the json response
 data = json.loads(response)
-# post the message to slack :)
-slack.chat.post_message('#general', "*_" + data['message'] + "_* " + data['subtitle'], username="FOAASbot", icon_emoji=":rage1:")
+# select two random numbers
+num1 = randint(1,10)
+num2 = randint(1,10)
+# if those numbers equal each other then post FO mesage
+if num1 == num2:
+        # post the message to slack :)
+        slack.chat.post_message('#general', "*_" + data['message'] + "_* " + data['subtitle'], username="FOAASbot", icon_emoji=":rage1:")
+# if not spread the love
+else:
+        slack.chat.post_message('#general', ":heart_eyes_cat:", username="FOAASbot", icon_emoji=":rage1:")
