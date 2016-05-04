9a1a30a321448acc9fa1a8aa1cf8bf3996d13370
Stephen Hilt <shooter000@gmail.com>
2016-05-03 21:17:27 -0400

    Create foaas_slack.py

---
+++ foaas_slack.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import os
+import json
+from slacker import Slacker
+from random import randint
+# setup slacker
+slack = Slacker('API_GOES_HERE')
+# list of commands that support from and to
+commands = ["off","you","donut","shakespeare","linus","king","chainsaw","outside","madison","nugget","yoda","caniuse","bus","xmas","bday","shutup"]
+# select the command to use
+command = commands[randint(0, len(commands)-1)]
+# get the list of slack users and select members 
+response = slack.users.list()
+users = response.body['members']
+# get the number of members
+numUsers = len(users)
+# select the user you want to randomly tell to FO
+fouser = randint(0, numUsers -1)
+# select the other use that the message will come from
+otheruser = randint(0, numUsers -1)
+# set up the url request and send it into curl
+url = "\'http://foaas.com/{}/@{}/{}\' -H \'Accept: application/json\'".format(command, users[fouser]['name'], users[otheruser]['name'])
+response = os.popen('curl -s {}'.format(url)).read()
+# pares the json response
+data = json.loads(response)
+# post the message to slack :)
+slack.chat.post_message('#general', "*_" + data['message'] + "_* " + data['subtitle'], username="FOAASbot", icon_emoji=":rage1:")
