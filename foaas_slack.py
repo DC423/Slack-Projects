6239ee71b66f91f98d68c28cda80b16b3ec62e0b
Stephen Hilt <shooter000@gmail.com>
2016-05-03 22:33:34 -0400

    Update foaas_slack.py
    added some logic to only choose active users.

--- foaas_slack.py
+++ foaas_slack.py
@@ -4,7 +4,8 @@ import json
 from slacker import Slacker
 from random import randint
 # setup slacker
-slack = Slacker('API_GOES_HERE')
+API = 'API_GOES_HERE'
+slack = Slacker(API)
 # list of commands that support from and to
 commands = ["off","you","donut","shakespeare","linus","king","chainsaw","outside","madison","nugget","yoda","caniuse","bus","xmas","bday","shutup"]
 # select the command to use
@@ -15,7 +16,15 @@ users = response.body['members']
 # get the number of members
 numUsers = len(users)
 # select the user you want to randomly tell to FO
-fouser = randint(0, numUsers -1)
+while True:
+        fouser = randint(0, numUsers -1)
+        url = "https://slack.com/api/users.getPresence?token={}&user={}&pretty=1".format(API, users[fouser]['id'])
+        response = os.popen('curl -s {}'.format(url)).read()
+        lastOnline = json.loads(response)
+        epoch = int(time.time())
+        # this should limit it to users online, or have been online in the last 5 min
+        if epoch > lastOnline['last_activity'] - 300:
+                break
 # select the other use that the message will come from
 otheruser = randint(0, numUsers -1)
 # set up the url request and send it into curl
