7443695175131d43fb19ff7e6d6f861d8faeb9a9
Stephen Hilt <shooter000@gmail.com>
2016-05-23 22:47:49 -0400

    Update foaas_slack.py
    upgrades to code, and added windows 10 message :)

--- foaas_slack.py
+++ foaas_slack.py
@@ -23,42 +23,43 @@ while True:
 	u = urllib.urlopen(url)
 	response = u.read()
 	presence = json.loads(response)
-	# if presense is active, and its not a bot we have our victim
-	if "active" in  presence['presence'] and "bot" not in users[fouser]['name']:
+	if "away" in  presence['presence'] and "bot" not in users[fouser]['name']:
 		break	
-# use the slack API to query open an IM to the user to send them the message later
-url = "https://slack.com/api/im.open?token={}&user={}&pretty=1".format(API, users[fouser]['id'])
-u = urllib.urlopen(url)
+
+api_im_open = "https://slack.com/api/im.open?token={}&user={}&pretty=1".format(API, users[fouser]['id'])
+u = urllib.urlopen(api_im_open)
 response = u.read()
 user_im = json.loads(response)
 im_id = user_im['channel']['id']
 
 # select the other use that the message will come from
 otheruser = randint(0, numUsers -1)
-# set up the url request and send it into curl
+# set up the url request
 url = "\'http://foaas.com/{}/@{}/{}\' -H \'Accept: application/json\'".format(command, users[fouser]['name'], users[otheruser]['name'])
 response = os.popen('curl -s {}'.format(url)).read()
 # pares the json response
 data = json.loads(response)
-# Roulette style 
+
 num1 = randint(1,6)
-num2 = randint(1,6)
-# If numbers are equal do something offinsive 
-if num1 == num2:
-	print "test"
+# if number is less than 3 than tell the user to F&#* off
+if num1 < 3:
 	# post the message to slack :)
-	slack.chat.post_message('#general', "*_" + data['message'] + "_* " + data['subtitle'], username="FOAASbot", icon_emoji=":rage1:")
-	slack.chat.post_message(im_id, "*_" + data['message'] + "_* " + data['subtitle'], username="FOAASbot", icon_emoji=":rage1:")
-else:
-	# User the username in a positive Chuck Norris esq joke
+	slack.chat.post_message('#general', "*_" + data['message'] + "_* ", username="FOAASbot", icon_emoji=":rage1:")
+#	slack.chat.post_message(im_id, "*_" + data['message'] + "_* " + data['subtitle'], username="FOAASbot", icon_emoji=":rage1:")
+# if number is greater than 3, then chuck noris joke the user.
+elif num1 > 3:
 	url = "http://api.icndb.com/jokes/random?firstName={}&lastName= ".format(users[fouser]['name'])
 	u = urllib.urlopen(url)
-	response = u.read()
+	response = os.popen('curl -s {}'.format(url)).read()
+	# response = u.read()
 	# pares the json response
 	data = json.loads(response)
-	slack.chat.post_message(im_id, data['value']['joke'].replace(" Norris",""), username="ChuckNorrisbot", icon_emoji=":chuck:")
-	slack.chat.post_message('#general', data['value']['joke'].replace(" Norris",""), username="ChuckNorrisbot", icon_emoji=":chuck:")
-# close the IM message
+	joke = data['value']['joke'].replace(" Norris","")
+	slack.chat.post_message('#general', joke.replace("&quot;","\""), username="ChuckNorrisbot", icon_emoji=":chuck:")
+#	slack.chat.post_message(im_id, data['value']['joke'].replace(" Norris",""), username="ChuckNorrisbot", icon_emoji=":chuck:")
+# else if the number is 3, then FREE Windows 10 Upgrade!!
+else: 
+	slack.chat.post_message('#general', users[fouser]['name'] + " Your FREE Windows 10 upgrade is ready!", as_user=True)
 url = "https://slack.com/api/im.close?token={}&user={}&pretty=1".format(API, users[fouser]['id'])
 u = urllib.urlopen(url)
 response = u.read()
