e920b7302caf3835e724b33a2a7eba486f2820e4
Stephen Hilt <shooter000@gmail.com>
2017-08-09 17:37:31 -0400

    Update delete_slack_files.py

--- delete_slack_files.py
+++ delete_slack_files.py
@@ -27,9 +27,10 @@ for usr in u['members']:
 if userid == 'NULL':
 	sys.exit()
 
-# list all files from user (only 100 at a time) 
+# Number of days
 days = 30
-urllist = 'https://slack.com/api/files.list?token={}&count=100&user={}&pretty=1'.format(token, userid, days)
+# list all files from user (only 100 at a time) 
+urllist = 'https://slack.com/api/files.list?token={}&count=100&ts_from={}&user={}&pretty=1'.format(token, userid, days)
 data = requests.get(urllist).json()
 # if there was files then parse the files and delete them 
 while data['ok'] == True:	
